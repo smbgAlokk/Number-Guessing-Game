07e9f0fbbccffd46faf4788dfed3001ee0a7781c
PasswordGenerator <contact.alokkumarr@gmail.com>
2025-06-04 16:38:48 +0530

    switch talk

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("jan");
+    break;
+  case 2:
+    console.log("fab");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+
+  default:
+    console.log("invalid month");
+    break;
+}
