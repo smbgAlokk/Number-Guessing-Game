7adf4a63928843eb314de1940586f7512f90d5e9
PasswordGenerator <contact.alokkumarr@gmail.com>
2025-06-05 15:38:41 +0530

    for in talks

---
+++ 05_iterations/four.js
@@ -0,0 +1,46 @@
+const myObject = {
+  js: "Javascript",
+  cpp: "C++",
+  rb: " ruby",
+  swift: "swift by apple",
+};
+
+for (const key in myObject) {
+  console.log(`${key} shortcut fot ${myObject[key]}`);
+}
+/*
+Output:
+
+js shortcut fot Javascript
+cpp shortcut fot C++
+rb shortcut fot  ruby
+swift shortcut fot swift by apple
+*/
+
+const programming = ["js", "rb", "py"];
+for (const key in programming) {
+  console.log(programming[key]);
+}
+/*
+Output:
+
+js
+rb
+py
+*/
+
+const map = new Map();
+map.set("IN", "India");
+map.set("Fr", "France");
+map.set("UK", "United Kingdom");
+
+for (const key in map) {
+  console.log(key);
+}
+
+/*
+Note:
+
+'for in' => Used for Objects 
+'for of' => Used for Arrays (mostly)
+*/
