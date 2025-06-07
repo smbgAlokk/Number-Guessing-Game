5a2853388c0b10e301f372c9550554aa9c9a48d4
PasswordGenerator <contact.alokkumarr@gmail.com>
2025-06-07 19:43:41 +0530

    .

--- 07_projects/03_digitalClock/script.js
+++
@@ -1,9 +0,0 @@
-const clock = document.getElementById("clock");
-
-setInterval(function () {
-  let date = new Date();
-  //   console.log(date.toLocaleTimeString());
-  clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
-
-// a function in JavaScript that is used to repeatedly execute a given function at a specified interval
